809716bc54b44a43ca570b4c2916d3e399c4707a
Vinícius Dantas <viniciusdantas@bct.ect.ufrn.br>
2020-03-10 16:40:06 -0300

    Add a storage genserver for keeping state

---
+++ server/lib/nfl/storage.ex
@@ -0,0 +1,30 @@
+defmodule Nfl.Storage do
+  use GenServer
+
+  def start_link do
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  def put(data) do
+    GenServer.cast(__MODULE__, {:put, data})
+  end
+
+  def get() do
+    GenServer.call(__MODULE__, :get)
+  end
+
+  @impl GenServer
+  def init(_) do
+    {:ok, nil}
+  end
+
+  @impl GenServer
+  def handle_cast({:put, data}, _store) do
+    {:noreply, data}
+  end
+
+  @impl GenServer
+  def handle_call(:get, _, store) do
+    {:reply, store, store}
+  end
+end
