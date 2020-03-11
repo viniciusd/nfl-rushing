4c9ee74248efee181c1d363904c35ea4000c7ab4
Vinícius Dantas <viniciusdantas@bct.ect.ufrn.br>
2020-03-11 18:26:35 -0300

    Apply formatter

--- server/test/nfl/players_test.exs
+++ server/test/nfl/players_test.exs
@@ -21,213 +21,213 @@ defmodule Nfl.PlayersTest do
     "FUM" => 0
   }
   @players [
-  %{
-    "Player" => "Joe Banyard",
-    "Team" => "JAX",
-    "Pos" => "RB",
-    "Att" => 2,
-    "Att/G" => 2,
-    "Yds" => 7,
-    "Avg" => 3.5,
-    "Yds/G" => 7,
-    "TD" => 0,
-    "Lng" => "7",
-    "1st" => 0,
-    "1st%" => 0,
-    "20+" => 0,
-    "40+" => 0,
-    "FUM" => 0
-  },
-  %{
-    "Player" => "Shaun Hill",
-    "Team" => "MIN",
-    "Pos" => "QB",
-    "Att" => 5,
-    "Att/G" => 1.7,
-    "Yds" => 5,
-    "Avg" => 1,
-    "Yds/G" => 1.7,
-    "TD" => 0,
-    "Lng" => "9",
-    "1st" => 0,
-    "1st%" => 0,
-    "20+" => 0,
-    "40+" => 0,
-    "FUM" => 0
-  },
-  %{
-    "Player" => "Breshad Perriman",
-    "Team" => "BAL",
-    "Pos" => "WR",
-    "Att" => 1,
-    "Att/G" => 0.1,
-    "Yds" => 2,
-    "Avg" => 2,
-    "Yds/G" => 0.1,
-    "TD" => 0,
-    "Lng" => "2",
-    "1st" => 0,
-    "1st%" => 0,
-    "20+" => 0,
-    "40+" => 0,
-    "FUM" => 0
-  },
-  %{
-    "Player" => "Charlie Whitehurst",
-    "Team" => "CLE",
-    "Pos" => "QB",
-    "Att" => 2,
-    "Att/G" => 2,
-    "Yds" => 1,
-    "Avg" => 0.5,
-    "Yds/G" => 1,
-    "TD" => 0,
-    "Lng" => "2",
-    "1st" => 0,
-    "1st%" => 0,
-    "20+" => 0,
-    "40+" => 0,
-    "FUM" => 0
-  },
-  %{
-    "Player" => "Lance Dunbar",
-    "Team" => "DAL",
-    "Pos" => "RB",
-    "Att" => 9,
-    "Att/G" => 0.7,
-    "Yds" => 31,
-    "Avg" => 3.4,
-    "Yds/G" => 2.4,
-    "TD" => 1,
-    "Lng" => "10",
-    "1st" => 3,
-    "1st%" => 33.3,
-    "20+" => 0,
-    "40+" => 0,
-    "FUM" => 0
-  },
-  %{
-    "Player" => "Mark Ingram",
-    "Team" => "NO",
-    "Pos" => "RB",
-    "Att" => 205,
-    "Att/G" => 12.8,
-    "Yds" => "1,043",
-    "Avg" => 5.1,
-    "Yds/G" => 65.2,
-    "TD" => 6,
-    "Lng" => "75T",
-    "1st" => 49,
-    "1st%" => 23.9,
-    "20+" => 4,
-    "40+" => 2,
-    "FUM" => 2
-  }
-]
+    %{
+      "Player" => "Joe Banyard",
+      "Team" => "JAX",
+      "Pos" => "RB",
+      "Att" => 2,
+      "Att/G" => 2,
+      "Yds" => 7,
+      "Avg" => 3.5,
+      "Yds/G" => 7,
+      "TD" => 0,
+      "Lng" => "7",
+      "1st" => 0,
+      "1st%" => 0,
+      "20+" => 0,
+      "40+" => 0,
+      "FUM" => 0
+    },
+    %{
+      "Player" => "Shaun Hill",
+      "Team" => "MIN",
+      "Pos" => "QB",
+      "Att" => 5,
+      "Att/G" => 1.7,
+      "Yds" => 5,
+      "Avg" => 1,
+      "Yds/G" => 1.7,
+      "TD" => 0,
+      "Lng" => "9",
+      "1st" => 0,
+      "1st%" => 0,
+      "20+" => 0,
+      "40+" => 0,
+      "FUM" => 0
+    },
+    %{
+      "Player" => "Breshad Perriman",
+      "Team" => "BAL",
+      "Pos" => "WR",
+      "Att" => 1,
+      "Att/G" => 0.1,
+      "Yds" => 2,
+      "Avg" => 2,
+      "Yds/G" => 0.1,
+      "TD" => 0,
+      "Lng" => "2",
+      "1st" => 0,
+      "1st%" => 0,
+      "20+" => 0,
+      "40+" => 0,
+      "FUM" => 0
+    },
+    %{
+      "Player" => "Charlie Whitehurst",
+      "Team" => "CLE",
+      "Pos" => "QB",
+      "Att" => 2,
+      "Att/G" => 2,
+      "Yds" => 1,
+      "Avg" => 0.5,
+      "Yds/G" => 1,
+      "TD" => 0,
+      "Lng" => "2",
+      "1st" => 0,
+      "1st%" => 0,
+      "20+" => 0,
+      "40+" => 0,
+      "FUM" => 0
+    },
+    %{
+      "Player" => "Lance Dunbar",
+      "Team" => "DAL",
+      "Pos" => "RB",
+      "Att" => 9,
+      "Att/G" => 0.7,
+      "Yds" => 31,
+      "Avg" => 3.4,
+      "Yds/G" => 2.4,
+      "TD" => 1,
+      "Lng" => "10",
+      "1st" => 3,
+      "1st%" => 33.3,
+      "20+" => 0,
+      "40+" => 0,
+      "FUM" => 0
+    },
+    %{
+      "Player" => "Mark Ingram",
+      "Team" => "NO",
+      "Pos" => "RB",
+      "Att" => 205,
+      "Att/G" => 12.8,
+      "Yds" => "1,043",
+      "Avg" => 5.1,
+      "Yds/G" => 65.2,
+      "TD" => 6,
+      "Lng" => "75T",
+      "1st" => 49,
+      "1st%" => 23.9,
+      "20+" => 4,
+      "40+" => 2,
+      "FUM" => 2
+    }
+  ]
   @sorted_players [
-  %{
-    "Player" => "Joe Banyard",
-    "Team" => "JAX",
-    "Pos" => "RB",
-    "Att" => 2,
-    "Att/G" => 2,
-    "Yds" => 7,
-    "Avg" => 3.5,
-    "Yds/G" => 7,
-    "TD" => 0,
-    "Lng" => "7",
-    "1st" => 0,
-    "1st%" => 0,
-    "20+" => 0,
-    "40+" => 0,
-    "FUM" => 0
-  },
-  %{
-    "Player" => "Shaun Hill",
-    "Team" => "MIN",
-    "Pos" => "QB",
-    "Att" => 5,
-    "Att/G" => 1.7,
-    "Yds" => 5,
-    "Avg" => 1,
-    "Yds/G" => 1.7,
-    "TD" => 0,
-    "Lng" => "9",
-    "1st" => 0,
-    "1st%" => 0,
-    "20+" => 0,
-    "40+" => 0,
-    "FUM" => 0
-  },
-  %{
-    "Player" => "Breshad Perriman",
-    "Team" => "BAL",
-    "Pos" => "WR",
-    "Att" => 1,
-    "Att/G" => 0.1,
-    "Yds" => 2,
-    "Avg" => 2,
-    "Yds/G" => 0.1,
-    "TD" => 0,
-    "Lng" => "2",
-    "1st" => 0,
-    "1st%" => 0,
-    "20+" => 0,
-    "40+" => 0,
-    "FUM" => 0
-  },
-  %{
-    "Player" => "Charlie Whitehurst",
-    "Team" => "CLE",
-    "Pos" => "QB",
-    "Att" => 2,
-    "Att/G" => 2,
-    "Yds" => 1,
-    "Avg" => 0.5,
-    "Yds/G" => 1,
-    "TD" => 0,
-    "Lng" => "2",
-    "1st" => 0,
-    "1st%" => 0,
-    "20+" => 0,
-    "40+" => 0,
-    "FUM" => 0
-  },
-  %{
-    "Player" => "Lance Dunbar",
-    "Team" => "DAL",
-    "Pos" => "RB",
-    "Att" => 9,
-    "Att/G" => 0.7,
-    "Yds" => 31,
-    "Avg" => 3.4,
-    "Yds/G" => 2.4,
-    "TD" => 1,
-    "Lng" => "10",
-    "1st" => 3,
-    "1st%" => 33.3,
-    "20+" => 0,
-    "40+" => 0,
-    "FUM" => 0
-  },
-  %{
-    "Player" => "Mark Ingram",
-    "Team" => "NO",
-    "Pos" => "RB",
-    "Att" => 205,
-    "Att/G" => 12.8,
-    "Yds" => "1,043",
-    "Avg" => 5.1,
-    "Yds/G" => 65.2,
-    "TD" => 6,
-    "Lng" => "75T",
-    "1st" => 49,
-    "1st%" => 23.9,
-    "20+" => 4,
-    "40+" => 2,
-    "FUM" => 2
-  }
-]
+    %{
+      "Player" => "Joe Banyard",
+      "Team" => "JAX",
+      "Pos" => "RB",
+      "Att" => 2,
+      "Att/G" => 2,
+      "Yds" => 7,
+      "Avg" => 3.5,
+      "Yds/G" => 7,
+      "TD" => 0,
+      "Lng" => "7",
+      "1st" => 0,
+      "1st%" => 0,
+      "20+" => 0,
+      "40+" => 0,
+      "FUM" => 0
+    },
+    %{
+      "Player" => "Shaun Hill",
+      "Team" => "MIN",
+      "Pos" => "QB",
+      "Att" => 5,
+      "Att/G" => 1.7,
+      "Yds" => 5,
+      "Avg" => 1,
+      "Yds/G" => 1.7,
+      "TD" => 0,
+      "Lng" => "9",
+      "1st" => 0,
+      "1st%" => 0,
+      "20+" => 0,
+      "40+" => 0,
+      "FUM" => 0
+    },
+    %{
+      "Player" => "Breshad Perriman",
+      "Team" => "BAL",
+      "Pos" => "WR",
+      "Att" => 1,
+      "Att/G" => 0.1,
+      "Yds" => 2,
+      "Avg" => 2,
+      "Yds/G" => 0.1,
+      "TD" => 0,
+      "Lng" => "2",
+      "1st" => 0,
+      "1st%" => 0,
+      "20+" => 0,
+      "40+" => 0,
+      "FUM" => 0
+    },
+    %{
+      "Player" => "Charlie Whitehurst",
+      "Team" => "CLE",
+      "Pos" => "QB",
+      "Att" => 2,
+      "Att/G" => 2,
+      "Yds" => 1,
+      "Avg" => 0.5,
+      "Yds/G" => 1,
+      "TD" => 0,
+      "Lng" => "2",
+      "1st" => 0,
+      "1st%" => 0,
+      "20+" => 0,
+      "40+" => 0,
+      "FUM" => 0
+    },
+    %{
+      "Player" => "Lance Dunbar",
+      "Team" => "DAL",
+      "Pos" => "RB",
+      "Att" => 9,
+      "Att/G" => 0.7,
+      "Yds" => 31,
+      "Avg" => 3.4,
+      "Yds/G" => 2.4,
+      "TD" => 1,
+      "Lng" => "10",
+      "1st" => 3,
+      "1st%" => 33.3,
+      "20+" => 0,
+      "40+" => 0,
+      "FUM" => 0
+    },
+    %{
+      "Player" => "Mark Ingram",
+      "Team" => "NO",
+      "Pos" => "RB",
+      "Att" => 205,
+      "Att/G" => 12.8,
+      "Yds" => "1,043",
+      "Avg" => 5.1,
+      "Yds/G" => 65.2,
+      "TD" => 6,
+      "Lng" => "75T",
+      "1st" => 49,
+      "1st%" => 23.9,
+      "20+" => 4,
+      "40+" => 2,
+      "FUM" => 2
+    }
+  ]
 
   test "list/0 returns all users" do
     assert Players.list() == @players
@@ -238,6 +238,7 @@ defmodule Nfl.PlayersTest do
       Players.list()
       |> Players.filter("Joe")
       |> Players.collect()
+
     assert result == [@joe]
   end
 
@@ -246,6 +247,7 @@ defmodule Nfl.PlayersTest do
       Players.list()
       |> Players.filter("joe")
       |> Players.collect()
+
     assert result == [@joe]
   end
 
@@ -254,6 +256,7 @@ defmodule Nfl.PlayersTest do
       Players.list()
       |> Players.sort("TD", "asc")
       |> Players.collect()
+
     assert result == @sorted_players
   end
 
@@ -263,10 +266,12 @@ defmodule Nfl.PlayersTest do
       |> Players.sort("TD", "desc")
       |> Players.collect()
       |> Enum.take(2)
+
     reversed =
       @sorted_players
       |> Enum.reverse()
       |> Enum.take(2)
+
     assert result == reversed
   end
 
@@ -275,9 +280,11 @@ defmodule Nfl.PlayersTest do
       Players.list()
       |> Players.paginate(1, 4)
       |> Players.collect()
+
     page =
       @players
       |> Enum.take(4)
+
     assert first_page == page
   end
 
@@ -286,6 +293,7 @@ defmodule Nfl.PlayersTest do
       Players.list()
       |> Players.paginate(1, 4)
       |> Players.collect()
+
     assert page_count == 2
   end
 end
