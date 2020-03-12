7385a4c6552377570952cbe4b34a06d68a293c65
Vinícius Dantas <viniciusdantas@bct.ect.ufrn.br>
2020-03-11 21:33:04 -0300

    Use an anchor instead of a form wrapping the download button

--- client/src/DownloadButton.js
+++ client/src/DownloadButton.js
@@ -3,11 +3,11 @@ import React from "react";
 function DownloadButton({ downloadUrl }) {
   return (
     <div className="d-flex w-100 justify-content-center btn-toolbar">
-      <form target="_blank">
-        <button className="btn btn-outline-secondary" formaction={downloadUrl}>
+      <a href={downloadUrl} target="_top">
+        <button className="btn btn-outline-secondary">
           Download
         </button>
-      </form>
+      </a>
     </div>
   );
 }
