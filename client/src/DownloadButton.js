b1bbbf9d7cdbeb5a03b67bb145db47d19bd28590
Vinícius Dantas <viniciusdantas@bct.ect.ufrn.br>
2020-03-11 22:52:37 -0300

    Apply formatter

--- client/src/DownloadButton.js
+++ client/src/DownloadButton.js
@@ -4,9 +4,7 @@ function DownloadButton({ downloadUrl }) {
   return (
     <div className="d-flex w-100 justify-content-center btn-toolbar">
       <a href={downloadUrl} target="_top">
-        <button className="btn btn-outline-secondary">
-          Download
-        </button>
+        <button className="btn btn-outline-secondary">Download</button>
       </a>
     </div>
   );
